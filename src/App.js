ad3d167661ee81b95632895b836969c9a7b93d9b
mewzih <ismailaouladtouhami18@gmail.com>
2022-07-26 12:07:59 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import Test from "./Test";
+function App() {
+  return (
+    <div className="App">
+        <Test/>
+    </div>
+  );
+}
+
+export default App;
